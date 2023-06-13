f1f7594fb302535b434bc4e23237f608ae6c858a
luzanovsky <luzanovsky@prb.team>
2023-06-13 11:05:41 +0300

    Changed db local path

--- aiogram_telegram_base_bot/bot/db.py
+++ aiogram_telegram_base_bot/bot/db.py
@@ -1,5 +1,6 @@
 from aiogram import types
 from typing import Optional
+import os
 
 from sqlalchemy import (
     Column, Integer, String, Boolean, ForeignKey, DateTime, func, select, insert, update, delete, literal_column,
@@ -18,7 +19,7 @@ engine: Optional[AsyncEngine] = None
 async def init_database():
     global engine
     if LOCAL_MODE:
-        DB_PATH = "/home/dmitriy/projects/deployment-planning-tool/db.sqlite3"
+        DB_PATH = os.path.join(os.path.dirname(__file__), "db.sqlite3")
         engine = create_async_engine(f"sqlite+aiosqlite:///{DB_PATH}", echo=True)
     else:
         engine = create_async_engine(DATABASE_URI, echo=True)
